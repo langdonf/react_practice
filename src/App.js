c944213f613c493bb48711e3bb4d7f9937ed6e41
Langdon <lfroker@gmail.com>
2019-04-27 09:45:34 -0700

    added delete and mark complete functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Make bed",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Do Laundry",
+        completed: false,
+      },
+      {
+        id: 4,
+        title: "Dishes",
+        completed: true,
+      }
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    }) })
+  }
+  delTodo = (id) =>{
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+  render(){
+    
+    
   return (
     <div className="App">
-     
+      <Todos markComplete={this.markComplete} delTodo={this.delTodo} todos={this.state.todos}/>
     </div>
   );
+  }
 }
 
 export default App;
