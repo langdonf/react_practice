a7c1140d422c9c60334e551894ea2176b0f0c57b
Langdon <lfroker@gmail.com>
2019-04-27 10:44:21 -0700

    added fake backend, react-router, and about page

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 class Todos extends Component{
     
     render(){
-        console.log(this.props.todos);
         return this.props.todos.map((todo)=>(
             <TodoItem key={todo.id} todo={todo} delTodo={this.props.delTodo} markComplete={this.props.markComplete}/>
         ))
